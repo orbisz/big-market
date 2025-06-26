a625c039a7a21793e6c859cd504f0d2d0b1c21b9
zxy <zxy0613zxy@outlook.com>
2025-06-26 14:30:49 +0800

    基础层持久化数据

---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IStrategyAwardDao.java
@@ -0,0 +1,21 @@
+package org.example.infrastructure.persistent.dao;
+
+import org.example.domain.strategy.model.entity.StrategyAwardEntity;
+import org.example.infrastructure.persistent.po.StrategyAward;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 抽奖策略奖品明细配置 - 概率、规则 DAO
+ * @create 2023-12-16 13:24
+ */
+@Mapper
+public interface IStrategyAwardDao {
+
+    List<StrategyAward> queryStrategyAwardList();
+
+    List<StrategyAward> queryStrategyAwardListByStrategyId(Long strategyId);
+
+}
---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IStrategyRuleDao.java
@@ -0,0 +1,19 @@
+package org.example.infrastructure.persistent.dao;
+
+import org.example.infrastructure.persistent.po.Strategy;
+import org.example.infrastructure.persistent.po.StrategyRule;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 策略规则 DAO
+ * @create 2023-12-16 13:25
+ */
+@Mapper
+public interface IStrategyRuleDao {
+
+    List<StrategyRule> queryStrategyRuleList();
+
+}
